1a5a831d2899a9f872a08afa80b3f9ebf0d2934d
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-05-20 14:39:24 -0400

    Added an example of using chrome storage in a content script.
    It just increments a counter every time a page loads.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,1 +1,13 @@
 ﻿console.log('Hello, world! This is the Chrome Extension Template speaking.');
+
+// ==================================
+// STORAGE
+// ==================================
+chrome.storage.local.get('count', function(data) {
+    var count = 1;
+    if (data.count) {
+        count = data.count + 1;
+    }
+    console.log('You have loaded ' + count + ' web page(s) since you have installed the Chrome Extension Template.');
+    chrome.storage.local.set({ 'count': count });
+});
