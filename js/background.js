2182962fc36c205b7cace1e5fe966e936f656579
Brian Anderson <banderson623@gmail.com>
2015-03-17 13:51:30 -0500

    initial pass at getting dfp trunc extension working

--- js/background.js
+++ js/background.js
@@ -3,17 +3,17 @@
 // ------------------------------------------------------------
 // https://developer.chrome.com/extensions/contextMenus
 // ============================================================
-chrome.contextMenus.create({
-    'title': 'Chrome Extension Template',
-    'contexts': ['page', 'selection', 'link', 'editable', 'image', 'video', 'audio'],
-    'onclick': function(info, tab) {
-        console.log('Right-Click Info');
-        console.log('-----------------');
-        console.log(info);
-        console.log(tab);
-        console.log('');
-    }
-});
+// chrome.contextMenus.create({
+//     'title': 'Chrome Extension Template',
+//     'contexts': ['page', 'selection', 'link', 'editable', 'image', 'video', 'audio'],
+//     'onclick': function(info, tab) {
+//         console.log('Right-Click Info');
+//         console.log('-----------------');
+//         console.log(info);
+//         console.log(tab);
+//         console.log('');
+//     }
+// });
 
 // ============================================================
 // MESSAGE PASSING
@@ -21,19 +21,60 @@ chrome.contextMenus.create({
 // https://developer.chrome.com/extensions/messaging
 // ============================================================
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log('Received a message!');
-        sendResponse({ 'message': 'Hello there, fine fellow!' });
-    }
+  function(request, sender, sendResponse) {
+    recordTruncationResult(request);
+    console.log('Received a message!', request, sender);
+  }
 );
+
+function recordTruncationResult(message){
+  if(message.truncated){
+    setTrunc();
+  } else {
+    setNonTrunc();
+  }
+
+  var key = 'dfp-trunc-history-v2';
+  try{
+    chrome.storage.local.get(key, function(history){
+      if(history && history.length > 0){
+        history = JSON.parse(history);
+      } else {
+        history = [];
+      }
+      history.push({'u':message.url,
+                    'r':message.request,
+                    'l':message.length,
+                    't':message.truncated
+                  });
+      chrome.storage.local.set({key: JSON.stringify(history)});
+    });
+  }catch(e){
+    console.log(e);
+  }
+}
 
 /**
  * Will retrieve the active tab in the current window.
- * @param  { Function } callback The first and only parameter of the callback will be 
+ * @param  { Function } callback The first and only parameter of the callback will be
  *                               the Tab object of the active tab in the current window.
  */
-function getActiveTab(callback) {
-    chrome.tabs.query({ 'active': true, 'currentWindow': true }, function(tabs) {
-        callback(tabs[0]);
-    });
+
+function setTrunc(){
+  var iconPath = 'img/icon-trunc.png';
+  chrome.browserAction.setIcon({path: iconPath});
 }
+
+function setNonTrunc(){
+  var iconPath = 'img/icon.png';
+  chrome.browserAction.setIcon({path: iconPath});
+}
+
+function getActiveTab(callback) {
+  chrome.tabs.query({ 'active': true, 'currentWindow': true }, function(tabs) {
+    callback(tabs[0]);
+    console.log('getActiveTab', callback);
+  });
+}
+
+
--- js/contentscript.js
+++ js/contentscript.js
@@ -1,25 +1,18 @@
-﻿console.log('Hello, world! This is the Chrome Extension Template speaking.');
+﻿console.log('DFP Truncation Extension is loading...', jQuery);
 
-// ============================================================
-// STORAGE
-// ------------------------------------------------------------
-// https://developer.chrome.com/extensions/storage
-// ============================================================
-chrome.storage.local.get('count', function(data) {
-    var count = 1;
-    if (data.count) {
-        count = data.count + 1;
-    }
-    console.log('You have loaded ' + count + ' web page(s) since you have installed the Chrome Extension Template.');
-    chrome.storage.local.set({ 'count': count });
-});
-
-// ============================================================
-// MESSAGE PASSING
-// ------------------------------------------------------------
-// https://developer.chrome.com/extensions/messaging
-// ============================================================
-chrome.runtime.sendMessage({ 'message': 'hello' }, function(response) {
-    // To see how this message was handled, see js/background.js.
-    console.log('The response from your message was: ' + response.message);
+jQuery(function($){
+  var $script = $('script[src*="gampad"]');
+  if($script.length){
+    var request = $script.attr('src');
+    var truncated = request.indexOf('trunc') > 0;
+    chrome.runtime.sendMessage(
+      {"truncated": truncated,
+       "length": request.length,
+       "request": request,
+       "url": location.href
+    },
+      function(response) {
+        console.log(response);
+      });
+  }
 });
